3418e162bb03741e9fd4d5c03903bf873de7c2f0
Jiahua Song <song@Jiahuas-MacBook-Pro.local>
2020-10-16 23:16:41 -0400

    add socket

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import './App.css';
+import socketIOClient from "socket.io-client";
 import Header from './components/Header/index';
 import Footer from './components/Footer/index';
 
+const url = "http://127.0.0.1:8080";
+
 class App extends React.Component {
+  componentDidMount() {
+    const socket = socketIOClient(url);
+    socket.emit('chat message', "client");
+    socket.on("chat message", data => {
+      console.log(data);
+    });
+
+    // CLEAN UP THE EFFECT
+    return () => socket.disconnect();
+  }
   render() {
     return (
       <div className="App">
