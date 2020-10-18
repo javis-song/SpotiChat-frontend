26e81128c71d22c010b09378cfaf5580eca73655
Yangyi Xu <lilimobs@gmail.com>
2020-10-17 23:45:32 -0400

    changed login button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,14 +4,19 @@ import logo from "./logo.svg";
 const Header = () => {
   return (
     <div className="App">
-      <header padding-top="10%" className="App-header">
+      <header padding-top="10%" className="App-header" margin="0">
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <hr width="100%"></hr>
+        <div width="100vp">
+          <hr width="100%"></hr>
 
-        <h1>Now Playing...</h1>
+          <h1 >Now Playing.......</h1>
 
 
-        <hr width="100%"></hr>
+          <hr width="100%"></hr>
+        </div>
+
+
+
 
         <iframe class="if" width="1000" height="50" src="https://editor.p5js.org/p5user1/embed/aOT3dzdd1"></iframe>
 
