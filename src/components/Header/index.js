a81bb53b35db13df99adc15c174aa9158d929b32
xiangyili <about.lxy@gmail.com>
2020-10-17 21:53:31 -0400

    add now playing header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,8 +5,10 @@ const Header = () => {
   return (
     <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+            <h1>Now Playing</h1>
         </header>
+
     </div>
         
   );
